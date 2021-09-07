cfc814a870264b524b666d999f7970417ff43f25
WangGossip <982728182@qq.com>
2021-09-07 21:26:42 +0800

    到了数据类型

---
+++ datatype.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print('''s1 is {} 
+s2 is {}
+s3 is {}
+s4 is {}'''.format(s1, s2, s3, s4))
+
+'''
+s1 is Hello, world 
+s2 is Hello, 'Adam'
+s3 is Hello, "Bart"
+s4 is Hello,
+Lisa!
+'''
